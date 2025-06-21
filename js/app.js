75fe20277175a14293788f6e685d6d0ba6848e59
Mario Waichert <waichert.mario@gmail.com>
2025-06-21 11:47:45 +0100

    lógica alterar status final

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //função para alterar o status ao clicar no botão
 function alterarStatus(id) {
+   //declaração de variáveis para modificar o necessário com base no index.html 
    let gameSelecionado = document.getElementById(`game-${id}`); //selecionar o game
    let imagem = gameSelecionado.querySelector('.dashboard__item__img');//selecionar o fundo
    let botaoAlugarDevolver = gameSelecionado.querySelector('.dashboard__item__button');//selecionar o botão Alugar ou Devolver
@@ -8,7 +9,7 @@ function alterarStatus(id) {
         imagem.classList.remove('dashboard__item__img--rented');//altera o fundo
         botaoAlugarDevolver.classList.remove('dashboard__item__button--return');//altera o botão
         botaoAlugarDevolver.textContent = 'Alugar';//altera o texto do botão
-
+     //lógica para a possibilidade contrária do que foi colocado acima
      } else {
         imagem.classList.add('dashboard__item__img--rented');
         botaoAlugarDevolver.classList.add('dashboard__item__button--return');
