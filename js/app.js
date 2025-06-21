4a2b3150c5db0c985a72a56c610c8a9167decc84
Mario Waichert <waichert.mario@gmail.com>
2025-06-21 11:44:57 +0100

    lógica da função alterar status

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
+//função para alterar o status ao clicar no botão
 function alterarStatus(id) {
-   let gameSelecionado = document.getElementById(`game-${id}`);
-   let imagem = gameSelecionado.querySelector('.dashboard__item__img');
-   let botaoAlugarDevolver = gameSelecionado.querySelector('.dashboard__item__button');
-
+   let gameSelecionado = document.getElementById(`game-${id}`); //selecionar o game
+   let imagem = gameSelecionado.querySelector('.dashboard__item__img');//selecionar o fundo
+   let botaoAlugarDevolver = gameSelecionado.querySelector('.dashboard__item__button');//selecionar o botão Alugar ou Devolver
+// condição para alterar os elementos necessários ao clicar o botão
      if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botaoAlugarDevolver.classList.remove('dashboard__item__button--return');
-        botaoAlugarDevolver.textContent = 'Alugar';
+        imagem.classList.remove('dashboard__item__img--rented');//altera o fundo
+        botaoAlugarDevolver.classList.remove('dashboard__item__button--return');//altera o botão
+        botaoAlugarDevolver.textContent = 'Alugar';//altera o texto do botão
 
      } else {
         imagem.classList.add('dashboard__item__img--rented');
