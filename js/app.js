e8934912039ca0670d19192a58bb1b8af513cb72
Mario Waichert <waichert.mario@gmail.com>
2025-07-04 15:56:36 +0100

    Adicionado a confirmação ao devolver o jogo.

--- js/app.js
+++ js/app.js
@@ -4,18 +4,20 @@ function alterarStatus(id) {
    let gameSelecionado = document.getElementById(`game-${id}`); //selecionar o game
    let imagem = gameSelecionado.querySelector('.dashboard__item__img');//selecionar o fundo
    let botaoAlugarDevolver = gameSelecionado.querySelector('.dashboard__item__button');//selecionar o botão Alugar ou Devolver
+   let nomeJogo = gameSelecionado.querySelector('.dashboard__item__name');
 // condição para alterar os elementos necessários ao clicar o botão
+     
      if (imagem.classList.contains('dashboard__item__img--rented')) {
+       if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
         imagem.classList.remove('dashboard__item__img--rented');//altera o fundo
         botaoAlugarDevolver.classList.remove('dashboard__item__button--return');//altera o botão
         botaoAlugarDevolver.textContent = 'Alugar';//altera o texto do botão
      //lógica para a possibilidade contrária do que foi colocado acima
+         }
      } else {
         imagem.classList.add('dashboard__item__img--rented');
         botaoAlugarDevolver.classList.add('dashboard__item__button--return');
         botaoAlugarDevolver.textContent = 'Devolver';
      }
-   
-}
 
-
+   }
